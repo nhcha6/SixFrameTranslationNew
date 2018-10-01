4ad6a000bc9e19567132e4581dcc39d15e0736fa
Arpit Bajaj <arpitbajaj98@gmail.com>
2018-10-02 01:21:26 +1000

    Got rid of calculator tutorial comments

--- DNAtoPep/SixFrameGUI.py
+++ DNAtoPep/SixFrameGUI.py
@@ -22,21 +22,6 @@ class Example(QWidget):
         self.grid = QGridLayout()
         self.setLayout(self.grid)
 
-        # names = ['Cls', 'Bck', '', 'Close',
-        #          '7', '8', '9', '/',
-        #          '4', '5', '6', '*',
-        #          '1', '2', '3', '-',
-        #          '0', '.', '=', '+']
-        #
-        # positions = [(i, j) for i in range(5) for j in range(4)]
-        #
-        # for position, name in zip(positions, names):
-        #
-        #     if name == '':
-        #         continue
-        #     button = QPushButton(name)
-        #     grid.addWidget(button, *position)
-
         self.initialiseWidgets()
 
         self.move(300, 150)
