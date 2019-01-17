3c6575d3a1f539c5c62f95c79e9c5753623a6c32
nhcha6 <nhcha6@student.monash.edu>
2019-01-17 11:14:58 +1100

    removeSubsetSeq now ensures peptide origins are carried through via using a dictionary. 1mil entries was brought down to 685893 entries in about 6 mins

--- DNAtoPep/removeSubsetSeq.py
+++ DNAtoPep/removeSubsetSeq.py
@@ -3,51 +3,79 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 from time import time
 
-def createSeqObj(matchedPeptides, transFlag):
+# create sequence object taken directly from the Mers code.
+def createSeqObj(seenPeptides, transFlag):
     """
     Given the set of matchedPeptides, converts all of them into SeqRecord objects and passes back a generator
     """
     count = 1
     seqRecords = []
-    for sequence in matchedPeptides:
+    try:
+        for sequence, name in seenPeptides.items():
+            yield SeqRecord(Seq(sequence), id=name, description="")
 
-        finalId = "ipd|pep"+str(count)+';'
-
-        yield SeqRecord(Seq(sequence), id=finalId, description="")
-
-        count += 1
-
+    except AttributeError:
+        for sequence in seenPeptides:
+            finalId = "ipd|pep" + str(count) + ';'
+            yield SeqRecord(Seq(sequence), id=finalId, description="")
+            count += 1
     return seqRecords
 
+
+# seenPeptides = []
+# with open('100,000-Record.fasta', "rU") as handle:
+#     counter = 0
+#     for record in SeqIO.parse(handle, 'fasta'):
+#         counter += 1
+#         seenPeptides.append(str(record.seq))
+
+time1 = time()
+
 seenPeptides = []
-with open('blahh2.fasta', "rU") as handle:
-    counter = 0
+with open('1mil.fasta', "rU") as handle:
+    origNo = 0
     for record in SeqIO.parse(handle, 'fasta'):
-        counter += 1
+        origNo += 1
         seenPeptides.append(str(record.seq))
 
-start = time()
+time2 = time()
+
+print('Number of original entries: ' + str(origNo))
+print('Time to read all entries to list: ' + str(time2-time1))
+
 seenPeptides.sort(key=len)
 seenPeptides.reverse()
 
 with open('sorted.fasta', "w") as output_handle:
     SeqIO.write(createSeqObj(seenPeptides, False), output_handle, "fasta")
 
-seenPeptides = set(seenPeptides)
+time3 = time()
+print('All sequences sorted and written to new dictionary in: ' + str(time3-time2))
+
+seenPeptides = {}
+with open('1mil.fasta', "rU") as handle:
+    for record in SeqIO.parse(handle, 'fasta'):
+        seenPeptides[str(record.seq)] = record.name
+
+time4 = time()
+print('Second read of input file took: ' + str(time4-time3))
 
 with open('sorted.fasta', "rU") as handle:
+    counter = 1
     for record in SeqIO.parse(handle, 'fasta'):
-        print(len(seenPeptides))
+        if counter % 10000 == 0:
+            print(counter)
+        counter+=1
         pep = str(record.seq)
-        if pep not in seenPeptides:
+        if pep not in seenPeptides.keys():
             continue
         for i in range(len(pep)):
             for j in range(i + 1, len(pep) + 1):
-                if pep[i:j] in seenPeptides and pep[i:j] is not pep:
-                    seenPeptides.remove(pep[i:j])
+                if pep[i:j] in seenPeptides.keys() and pep[i:j] is not pep:
+                    del seenPeptides[pep[i:j]]
 
-print(time()-start)
-print(len(seenPeptides))
+print('Time to delete subset sequences: ' + str(time()-time4))
+print('No. of sequences reduced from ' + str(origNo) + ' to ' + str(len(seenPeptides)))
 
 with open('Output.fasta', "w") as output_handle:
     SeqIO.write(createSeqObj(seenPeptides, False), output_handle, "fasta")
